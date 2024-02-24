edcd32b6bd0e1fde5d27aa64f605e4847d847841
OlgaPavlenko <olgaigpavlenko@gmail.com>
2024-02-24 12:43:10 +0000

    added dist

---
+++ portfolio.f4c2eb73.js
@@ -0,0 +1,2 @@
+(()=>{let e=document.querySelector("[data-menu-button]"),t=document.querySelector("[data-menu]");e.addEventListener("click",()=>{let a="true"===e.getAttribute("aria-expanded")||"false";e.classList.toggle("is-open"),e.setAttribute("aria-expanded",!a),t.classList.toggle("is-open")})})();
+//# sourceMappingURL=portfolio.f4c2eb73.js.map
