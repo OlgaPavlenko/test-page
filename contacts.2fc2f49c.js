483e4c495a0d0c176cee6ca863ee2098985c87c6
OlgaPavlenko <olgaigpavlenko@gmail.com>
2024-02-25 13:14:21 +0000

    dist

--- contacts.2fc2f49c.js
+++
@@ -1,2 +0,0 @@
-(()=>{let e=document.querySelector("[data-menu-button]"),t=document.querySelector("[data-menu]");e.addEventListener("click",()=>{let a="true"===e.getAttribute("aria-expanded")||"false";e.classList.toggle("is-open"),e.setAttribute("aria-expanded",!a),t.classList.toggle("is-open")})})();
-//# sourceMappingURL=contacts.2fc2f49c.js.map
